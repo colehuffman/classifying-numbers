aa9c2f750977d03d79c058d9a7e79bbbdb995bc0
colehuffman <79929352+colehuffman@users.noreply.github.com>
2021-03-17 23:05:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,11 +8,11 @@ input.onButtonPressed(Button.A, function () {
 // If button A and B are pressed, the if statements are triggered
 input.onButtonPressed(Button.AB, function () {
     // If the variable is equal to 0 a Z will be displayed
-    // If the variable is greater than or equal to 1 a P will be displayed
+    // If the variable is greater than 0  a P will be displayed
     // If the variable is less than 0 a N will be displayed
     if (number == 0) {
         basic.showString("Z")
-    } else if (number >= 1) {
+    } else if (number > 0) {
         basic.showString("P")
     } else if (number < 0) {
         basic.showString("N")
@@ -21,6 +21,12 @@ input.onButtonPressed(Button.AB, function () {
 // When button B is pressed the variable decreases by 1
 input.onButtonPressed(Button.B, function () {
     number += -1
+    // displays the number
+    basic.showString("" + (number))
+})
+input.onGesture(Gesture.Shake, function () {
+    // when shaken the number switches from a positive to a negative or from a negative to a positive.
+    number = 0 - number
     // displays the number
     basic.showString("" + (number))
 })
